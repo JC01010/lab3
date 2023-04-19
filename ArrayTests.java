aa53c09155d6a43d215b75453d4bfbcc6165e523
Jason Chang <jsc008@ucsd.edu>
2023-04-19 16:27:26 -0700

    Update ArrayTests.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -2,33 +2,33 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTests {
-	@Test 
-	public void testReverseInPlace() {
-    int[] input1 = { 3 };
-    ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
-	}
+	// @Test 
+	// public void testReverseInPlace() {
+  //   int[] input1 = { 3 };
+  //   ArrayExamples.reverseInPlace(input1);
+  //   assertArrayEquals(new int[]{ 3 }, input1);
+	// }
 
 
-  @Test
-  public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
-  }
+  // @Test
+  // public void testReversed() {
+  //   int[] input1 = { };
+  //   assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  // }
 
-	@Test 
-	public void testReverseInPlaceTwo() {
-    int[] input1 = { 3, 4, 5 };
-    ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 5, 4, 3 }, input1);
-	}
+	// @Test 
+	// public void testReverseInPlaceTwo() {
+  //   int[] input1 = { 3, 4, 5 };
+  //   ArrayExamples.reverseInPlace(input1);
+  //   assertArrayEquals(new int[]{ 5, 4, 3 }, input1);
+	// }
 
-	@Test 
-	public void testReverseInPlaceThree() {
-    int[] input1 = { 6, 7, 8, 9, 10 };
-    ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 10, 9, 8, 7, 6 }, input1);
-	}
+	// @Test 
+	// public void testReverseInPlaceThree() {
+  //   int[] input1 = { 6, 7, 8, 9, 10 };
+  //   ArrayExamples.reverseInPlace(input1);
+  //   assertArrayEquals(new int[]{ 10, 9, 8, 7, 6 }, input1);
+	// }
 
 
 	@Test 
